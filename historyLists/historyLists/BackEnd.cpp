972ba3efd77b8c164539f2bd4991f7e3d8fd6197
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2022-01-27 08:57:04 +0200

    Make the logic of a few methods

--- historyLists/historyLists/BackEnd.cpp
+++ historyLists/historyLists/BackEnd.cpp
@@ -5,3 +5,47 @@
 
 using namespace std;
 
+bool EventsList::MoveToNext() 
+{
+	if ((current == nullptr) || (current->next == nullptr))
+		return false;
+
+	current = current->next;
+	return true;
+}
+
+bool EventsList::MoveToPrev()
+{
+	if ((current == nullptr) || (current->prev == nullptr))
+		return false;
+
+	current = current->prev;
+	return true;
+}
+
+void EventsList::PushBack(DATA d)
+{
+	ELEMENT* e = new ELEMENT;
+	e->data = d; //copy the data
+	e->prev = last; // The previous element becomes last in the list
+	e->next = nullptr; // The new element becomes last in the list
+
+	if (first == nullptr) //checks if we add an element in an empty list
+		first = e; //if true -> the new element becomes first in the list
+
+	last = e; //the new element becomes the last in the list
+}
+
+void EventsList::PushFront(DATA d)
+{
+	ELEMENT* e = new ELEMENT;
+	e->data = d;
+
+	e->prev = nullptr;
+	e->next = first;
+
+	if (last == nullptr)
+		last = e;
+
+	first = e;
+}

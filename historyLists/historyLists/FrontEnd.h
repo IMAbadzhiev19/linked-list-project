b4cc4db068383040d12a7ec1d888114a31608dd2
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2022-02-06 16:15:17 +0200

    Add class for menu

--- historyLists/historyLists/FrontEnd.h
+++ historyLists/historyLists/FrontEnd.h
@@ -1,1 +1,24 @@
 #pragma once
+#include"BackEnd.h"
+
+class Menu
+{
+public:
+	Menu() { list.LoadFromFile(fileName); }
+	~Menu() { system("cls"); }
+
+	void clearScreen();
+	void gotoXY();
+
+	void displayOptions();
+	void displayList();
+	void pushBack();
+	void pusFront();
+	void findEvents();
+	void changeInfo();
+	void deleteEvents();
+
+private:
+	EventsList list;
+	string fileName = "history";
+};

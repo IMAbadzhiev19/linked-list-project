8028de6353b0166e1b0667cb62c126c51cff44a4
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2022-02-06 16:22:27 +0200

    Add method to display all events

--- historyLists/historyLists/FrontEnd.cpp
+++ historyLists/historyLists/FrontEnd.cpp
@@ -25,3 +25,45 @@ void Menu::gotoXY(int x, int y)
 
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coords);
 }
+
+void Menu::displayOptions()
+{
+
+}
+
+void Menu::displayList()
+{
+	EventsList::DATA data1;
+	list.MoveToFirst();
+
+	while (list.GetData(data1) == true)
+	{
+		cout << data1.year << '-' << data1.month << '-' << data1.day << "  " << data1.subject << " " << data1.leader << " " << data1.place << endl;
+		list.MoveToNext();
+	}
+}
+
+void Menu::pushBack()
+{
+
+}
+
+void Menu::pushFront()
+{
+
+}
+
+void Menu::findEvents()
+{
+
+}
+
+void Menu::changeInfo()
+{
+
+}
+
+void Menu::deleteEvents()
+{
+
+}
